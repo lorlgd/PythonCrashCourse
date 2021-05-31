d39aa966e1d32682546be973f58ff617f88e28b3
Javier Ramirez <lorlgd1@gmail.com>
2021-05-30 19:40:16 -0500

    JaRa: Added settings.py file to track settings for the game

--- Projects/alien_invasion/alien_invasion.py
+++ Projects/alien_invasion/alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from Projects.alien_invasion.settings import Settings
 
 class AlienInvassion:
     """Overall class to manage game assets and behavior"""
@@ -7,8 +8,11 @@ class AlienInvassion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
@@ -23,8 +27,8 @@ class AlienInvassion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
-            
+            self.screen.fill(self.settings.bg_color)
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
