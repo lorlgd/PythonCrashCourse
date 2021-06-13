84482a7e7a2db52f676a7c0ce86f4e7177d13306
Javier Ramirez <lorlgd1@gmail.com>
2021-06-13 16:51:35 -0500

    JaRa: Added game stats, collisions handling and attempts.

--- Projects/alien_invasion/settings.py
+++ Projects/alien_invasion/settings.py
@@ -8,17 +8,18 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_width = 30
         self.bullet_height = 15
         self.bullet_color = (210, 1, 46)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.alien_speed = 0.05
+        self.fleet_drop_speed = 100
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
